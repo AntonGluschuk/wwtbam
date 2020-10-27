208739951d5f6f35d77f4fc2ac1b3de27937b242
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-10-27 10:39:36 +0200

    Updates

---
+++ precache-manifest.2437130b4fa9bc4e2b701b7976cbf38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f3eaa1209349088447a6b81bc72b3f",
+    "url": "/wwtbam_app/index.html"
+  },
+  {
+    "revision": "764264a20681a2cee84d",
+    "url": "/wwtbam_app/static/css/main.eca85243.chunk.css"
+  },
+  {
+    "revision": "3bc745d8c84f3fb51d48",
+    "url": "/wwtbam_app/static/js/2.2b876eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wwtbam_app/static/js/2.2b876eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764264a20681a2cee84d",
+    "url": "/wwtbam_app/static/js/main.e23d141f.chunk.js"
+  },
+  {
+    "revision": "a8a3be253337ed684efb",
+    "url": "/wwtbam_app/static/js/runtime-main.4729f234.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/wwtbam_app/static/media/hand1.f8c75481.png"
+  }
+]);
